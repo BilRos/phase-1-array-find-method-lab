031b6a0bf4b531612f6fb923c04e51465f3ae13f
BilRos <billyrossowuor@gmail.com>
2024-04-01 16:55:36 +0300

    Find objects in an objects ... Still not fully done or understood

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+
+/*
+function isOdd(element, index, array) {
+    return (element % 2 === 1);
+  }
+  
+  [4, 6, 8, 10].find(isOdd); //=> undefined, not found
+  [4, 5, 8, 10].find(isOdd); //=> 5
+  [4, 5, 7, 8, 10].find(isOdd); //=> 5
+  [4, 7, 5,  8, 10].find(isOdd); //=> 7
+  
+*/
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+]
+
+function superbowlWin(array){
+    
+   for (const obj in array){
+    if(obj.result === "W"){
+    console.log(obj.year);
+    }
+    else {
+        return "sadly", undefined ;
+    }
+   } 
+}
+superbowlWin(record)
+
